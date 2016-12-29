dac18c23aa4b2b8daaaae3663d3d1c50f569d2fa
Al Prudeaux <aprudeaux@peerpointcapital.com>
2016-12-29 17:43:24 +0000

    Added basic pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def careers
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+
+  def knowledge
+  end
+
+  def products
+  end
+  
+  def blog
+  end        
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'pages#home'
+
+  get 'about' => 'pages#about'
+
+  get 'careers' => 'pages#careers'
+
+  get 'contact' => 'pages#contact'
+
+  get 'privacy' => 'pages#privacy'
+
+  get 'terms' => 'pages#terms'
+
+  get 'knowledge' => 'pages#knowledge'
+  
+  get 'products' => 'pages#products'
+  
+  get 'blog' => 'pages#blog'
+
 end
