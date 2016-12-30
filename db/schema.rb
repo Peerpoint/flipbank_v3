4e0ed3af2663e919aeb4b8c6aef631d5fa66bd0f
Al Prudeaux <aprudeaux@peerpointcapital.com>
2016-12-30 17:00:33 +0000

    small mod to schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20161230163220) do
   create_table "contacts", force: :cascade do |t|
     t.string    "name"
     t.string    "email"
-    t.text    "comments"
+    t.text      "comments"
     t.datetime  "created_at",         null: false
     t.datetime  "updated_at",         null: false
   end
